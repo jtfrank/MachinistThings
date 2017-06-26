90b0887d03e859423022e30f7c0b41aadd22bda3
jtfrank <aqualis@gmail.com>
2017-06-25 23:25:54 -0500

    Trying to reading text file asset

--- app/src/main/java/me/joshfrank/machinistthings/MainActivity.java
+++ app/src/main/java/me/joshfrank/machinistthings/MainActivity.java
@@ -1,18 +1,39 @@
 package me.joshfrank.machinistthings;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    String[] drillSizes = {"1/8 .1250\"", "3/16 .1875\"", "1/4 .2500\""};
+    ArrayList<String> drillSizes = new ArrayList<String>();
+    AssetManager am = this.getAssets();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            InputStream is = am.open("drillbit_sizes.txt");
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            while((line = br.readLine()) != null){
+                drillSizes.add(line);
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, drillSizes);
 
